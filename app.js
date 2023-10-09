7ad146ace92339b906a70a53ae972ff25bc43af2
Shreyash Daundkar <productiveappa@gmail.com>
2023-10-09 09:44:35 +0530

    Added mysql database setup with sequelize

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
+const database = require('./util/database');
+
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json({extended: false}));
 
-app.listen(4000);
+database.sync().then(() => app.listen(4000));
---
+++ util/database.js
@@ -0,0 +1,3 @@
+const Sequelize = require('sequelize');
+
+module.exports = new Sequelize('appointments', 'root', '9763387137', {dialect: 'mysql', host: 'localhost'});
